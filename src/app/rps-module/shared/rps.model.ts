cb2e775be6f1eb8c6143df4895357b78bfdfd578
Joost Zöllner <joostzoellner@online.de>
2018-04-03 19:37:13 +0200

    Add rsp module and model

---
+++ src/app/rps-module/shared/rps.model.ts
@@ -0,0 +1,11 @@
+export enum RockPaperScissors {
+    Rock = 'ROCK',
+    Paper = 'PAPER',
+    Scissors = 'Scissors'
+}
+
+export enum Result {
+    Won = 'WON',
+    Lost = 'LOST',
+    Draw = 'DRAW'
+}
