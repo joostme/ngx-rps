ef87cb13fc707e78bebbfb4984896b7a98204e7f
Joost Zöllner <joostzoellner@online.de>
2018-04-04 20:18:39 +0200

    Add computer result

--- src/app/rps-module/components/board/result/result.component.ts
+++ src/app/rps-module/components/board/result/result.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { GameState, Result, resultDescriptions } from 'app/rps-module/shared/rps.model';
+import { GameState, Result, RockPaperScissors, resultDescriptions } from 'app/rps-module/shared/rps.model';
 import { isNil } from 'lodash';
 
 @Component({
@@ -12,6 +12,7 @@ export class ResultComponent {
     @Input()
     gameState: GameState;
 
+    rockPaperScissors = RockPaperScissors;
     resultDescriptions = resultDescriptions;
 
     get result(): string {
@@ -19,5 +20,16 @@ export class ResultComponent {
             return this.resultDescriptions[this.gameState.result];
         }
         return 'Play a game!';
+    }
+
+    get computerHand(): string {
+        if (!isNil(this.gameState)) {
+            return this.gameState.computer;
+        }
+        return null;
+    }
+
+    private isOption(option: RockPaperScissors): boolean {
+        return this.computerHand === option;
     }
 }
