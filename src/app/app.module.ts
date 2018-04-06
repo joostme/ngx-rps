d2047f8bc0a20e23befd3d5adac22bd871017dc7
Joost Zöllner <joostzoellner@online.de>
2018-04-06 17:59:36 +0200

    Add service-worker config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
-
+import { environment } from 'environments/environment';
 import { AppComponent } from './app.component';
 import { RpsModule } from './rps-module/rps.module';
 
@@ -12,6 +13,7 @@ import { RpsModule } from './rps-module/rps.module';
     ],
     imports: [
         BrowserModule,
+        ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
         RpsModule
     ],
     providers: [],
